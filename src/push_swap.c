6185c873f1999653606b4d8c6f0a5937cbdf41f6
Louis Bastien <lbastien@car8s1.42barcelona.com>
2023-04-17 18:42:00 +0200

    trying insert

--- src/push_swap.c
+++ src/push_swap.c
@@ -18,9 +18,85 @@ int	main(int argc, char **argv)
 	stack = ft_parse_input(argc, argv);
 	if (!stack)
 		return (0);
-	stack = ft_quicksort(stack);
+	stack = insertsort(stack);
 	ft_print_lst(stack);
 	return (0);
+}
+
+int_list	*ft_insertsort(int_list *stack)
+{
+	int_list *head;
+	int_list *stackb;
+
+	stackb = NULL;
+	head = stack;
+	while (head != NULL)
+	{
+		do_pb(head, stackb);
+		ft_insert(stackb);
+	}
+}
+
+void *ft_insert(int_list *stack)
+{
+	int_list	*head;
+	int			insert_index;
+	int			count_pb;
+
+	head = stack;
+	insert_index = ft_find_index(stack);
+	count = 0;
+	while(insert_index)
+	{
+		do_sb(stack);
+		if (i != 1)
+		{
+			do_rb(stack);
+			count++;
+		}
+		i--;
+	}
+	while(count--)
+		do_rrb(stack);
+}
+
+int		ft_find_index(int_list *stack)
+{
+		int_list	*check;
+		int			*value;
+		int			i;
+		
+		check = stack->next;
+		value = stack->num;
+		i = 0;
+		while(check)
+		{
+			if(value > check->num)
+				return (i);
+			check = check->next;
+			i++;
+		}
+		return(i);
+}
+
+void	*do_pb(int_list *stacka, int_list *stackb)
+{
+	int_list	*node;
+	
+	node = ft_intlst_new(stacka->num);
+	ft_intlst_addfront(&stackb, node);
+	ft_printf("pb\n");
+	ft_freetop(stacka);
+}
+
+void	*do_pb(int_list *stacka, int_list *stackb)
+{
+	int_list	*node;
+	
+	node = ft_intlst_new(stacka->num);
+	ft_intlst_addfront(&stackb, node);
+	ft_printf("pb\n");
+	ft_freetop(stacka);
 }
 
 int_list	*ft_quicksort(int_list	*stack)
@@ -87,12 +163,11 @@ int_list	*pa(int_list **stacka, int_list *stackb)
 	return (stackb);
 }
 
-int_list *ft_freetop(int_list *lst)
+void *ft_freetop(int_list *lst)
 {
 	int_list	*tmp;
 
 	tmp = lst;
 	lst = lst->next;
 	free(tmp);
-	return (lst);
 }
